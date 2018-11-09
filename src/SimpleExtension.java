99a80ea77456f3405092352ef2e8925cc5b41158
tgolnoft <tgolnoft@gmail.com>
2018-11-08 23:28:19 -0700

    Rolling along on Simple

--- src/SimpleExtension.java
+++ src/SimpleExtension.java
@@ -2,15 +2,15 @@ package SimpleExtension;
 
 import bluej.extensions.*;
 import bluej.extensions.event.*;
+import javax.swing.*;
 import java.net.URL;
+import java.awt.event.*;
 
 public class SimpleExtension extends Extension implements PackageListener{
-    MenuBuilder myMenus;
     public void startup(BlueJ bluej){
         //listen for bluej events at the package level
         bluej.addPackageListener(this);
-        myMenus = new MenuBuilder();
-        bluej.setMenuGenerator(myMenus);
+        bluej.setMenuGenerator(new MenuBuilder());
     }
 
     //this runs when bluej detects that the user has "opened a package".
@@ -52,4 +52,89 @@ public class SimpleExtension extends Extension implements PackageListener{
             return null;
         }
     }
+
+
+    class MenuBuilder extends MenuGenerator{
+    //B for bluej, current package, class, and object 
+    //being edited
+    private BPackage curPackage;
+    private BClass curClass;
+    private BObject curObject;
+
+    public JMenuItem getToolsMenuItem(BPackage aPackage)
+    {
+        return new JMenuItem(new SimpleAction("Click Tools", "Tools menu:"));
+    }
+
+    public JMenuItem getClassMenuItem(BClass aClass)
+    {
+        return new JMenuItem(new SimpleAction("Click Class", "Class menu:"));
+    }
+
+    public JMenuItem getObjectMenuItem(BObject anObject)
+    {
+        return new JMenuItem(new SimpleAction("Click Object", "Object menu:"));
+    }
+
+    // These methods will be called when
+    // each of the different menus are about to be invoked.
+    public void notifyPostToolsMenu(BPackage bp, JMenuItem jmi)
+    {
+        System.out.println("Post on Tools menu");
+        curPackage = bp ; curClass = null ; curObject = null;
+    }
+
+    public void notifyPostClassMenu(BClass bc, JMenuItem jmi)
+    {
+        System.out.println("Post on Class menu");
+        curPackage = null ; curClass = bc ; curObject = null;
+    }
+
+    public void notifyPostObjectMenu(BObject bo, JMenuItem jmi)
+    {
+        System.out.println("Post on Object menu");
+        curPackage = null ; curClass = null ; curObject = bo;
+    }
+
+    // A utility method which pops up a dialog detailing the objects                
+    // involved in the current (SimpleAction) menu invocation.
+    private void showCurrentStatus(String header)
+    {
+        try
+        {
+            if (curObject != null)
+                curClass = curObject.getBClass();
+            if (curClass != null)
+                curPackage = curClass.getPackage();
+
+            String msg = header;
+            if (curPackage != null)
+                msg += "\nCurrent Package = " + curPackage;
+            if (curClass != null)
+                msg += "\nCurrent Class = " + curClass;
+            if (curObject != null)
+                msg += "\nCurrent Object = " + curObject;
+            JOptionPane.showMessageDialog(null, msg);
+        }
+        catch (Exception exc)
+        {
+        }
+    }
+
+    // The nested class that instantiates the different (simple) menus.
+    @SuppressWarnings("serial")
+    class SimpleAction extends AbstractAction {
+        private String msgHeader;
+
+        public SimpleAction(String menuName, String msg)
+        {
+            putValue(AbstractAction.NAME, menuName);
+            msgHeader = msg;
+        }
+        public void actionPerformed(ActionEvent anEvent)
+        {
+            showCurrentStatus(msgHeader);
+        }
+    }
+}
 }
