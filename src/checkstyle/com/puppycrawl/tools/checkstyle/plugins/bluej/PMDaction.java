682985d3155017ff584fedf0c7a035af979f303a
James Niedfeldt <37161212+JamesNiedfeldt@users.noreply.github.com>
2018-11-11 11:48:31 -0700

    Pmd (#3)
    * added travis doc
    
    * modifying travis file
    
    * ...
    
    * ...
    
    * ...
    
    * working on dev
    
    * Started Simple Extension
    
    * work on simpleext
    
    * Rolling along on Simple
    
    * Remove menubuilder class
    
    * finished simple extension with menus
    
    * switching to checkstyle environment
    
    * starting point for PMD
    
    * tweaks to build and PMD, TODO: build still broken...
    
    * fixed build. added PMD-core to lib and pointed the build file at it

---
+++ src/checkstyle/com/puppycrawl/tools/checkstyle/plugins/bluej/PMDaction.java
@@ -0,0 +1,16 @@
+package com.puppycrawl.tools.checkstyle.plugins.bluej;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.AbstractAction;
+import net.sourceforge.pmd.PMDConfiguration;
+
+
+class PMDaction extends AbstractAction {
+
+    public void actionPerformed(ActionEvent event){
+        //TODO: Collect all java files worked on
+        //TODO: Generate PMD preferences
+        //TODO: Generate PMD reports
+    }
+}
