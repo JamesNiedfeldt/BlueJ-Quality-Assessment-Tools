6af97ddcfd2abdedaec0ddf9fdb56862a07efd6e
jamesniedfeldt <jamesniedfeldt@gmail.com>
2018-11-15 11:07:58 -0700

    add testing environment

---
+++ src/tests/AntTest.java
@@ -0,0 +1,13 @@
+import org.junit.*;
+
+public class AntTest() {
+  @Test
+  public void doThing() {
+    assertEquals(1,1);
+  }
+  
+  @Test
+  public void doOtherThing() {
+    assertEquals(1,2);
+  }
+}
