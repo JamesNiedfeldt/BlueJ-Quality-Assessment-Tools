9cdff80f72a742fa77d4d37bfbb7f9cdef72fa8d
jamesniedfeldt <jamesniedfeldt@gmail.com>
2018-12-01 13:10:03 -0700

    Made NOT_FINISHED PMD message more helpful

--- src/com/tools/pmd/PMD_Report.java
+++ src/com/tools/pmd/PMD_Report.java
@@ -1,18 +1,22 @@
 package com.tools.pmd;
 
+/** Used by PMD_Report_Builder to construct the PMD message */
 public class PMD_Report {
   private StringBuilder msg;
   private boolean finalized;
   private static final String LINE_SEPARATOR = System.getProperty("line.separator");
   private static final String EXPLANATION = "Any problems found are displayed below:";
-  private static final String NOT_FINISHED = "The PMD report is not finished yet.";
+  private static final String NOT_FINISHED = "The PMD report is not ready yet. "+
+  "It will be ready after you compile and after the PMD checks are finished.";
   
+  /**Constructor, readys the StringBuilder with EXPLANATION as header */
   public PMD_Report(){
     msg = new StringBuilder(EXPLANATION);
     msg.append(LINE_SEPARATOR);
     finalized = false;
   }
   
+  /** Adds a line to the message if not finalized */
   public void appendMessage(String input){
     if(!finalized){
       msg.append(input);
@@ -20,6 +24,7 @@ public class PMD_Report {
     }
   }
   
+  /** Returns NOT_FINISHED if report is not finalized, otherwise returns message */
   public String getMessage(){
     if(!finalized){
       return NOT_FINISHED;
@@ -28,6 +33,7 @@ public class PMD_Report {
     }
   }
   
+  /** Finalize the report so it is ready to display. It cannot be edited further. */
   public void finalize(){
     finalized = true;
   }
